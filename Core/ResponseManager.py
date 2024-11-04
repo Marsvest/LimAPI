7e5ae28d1ea32ead8154344aaa79ef27227abeee
Marsvest <alajh.akbar@gmail.com>
2024-11-04 22:11:24 +1000

    feat: :construction: response manager template
    response manager template

--- Core/ResponseManager.py
+++ Core/ResponseManager.py
@@ -1,3 +1,28 @@
+from typing import Any
+
+from Core.Types import Response
+
+
 class ResponseManager:
-    async def process():
+    @classmethod
+    async def process(self, data: Any | Response) -> Response:
+        if type(data) is Response:
+            response: Response = await self.add_require_fields(data)
+        else:
+            response: Response = await self.create_from_empty(data)
+
+        cooked_response: str = await self.stringify(response)
+        
+        return cooked_response
+
+    @classmethod
+    async def create_from_empty(self, data: Any) -> Response:
+        pass
+
+    @classmethod
+    async def add_require_fields(self, response: Response) -> Response:
+        pass
+
+    @classmethod
+    async def stringify(self, response: Response) -> str:
         pass
